8a7a0e4a1c95c1fb20f6228c3016ec03299335a7
Supu12-ui <76004904+Supu12-ui@users.noreply.github.com>
2021-12-01 21:35:37 +0530

    Add files via upload

--- Day2function.java
+++ Day2function.java
@@ -82,15 +82,120 @@ public class Day2function {
     {
         return (a*b)/gcd(a,b);
     }
-    int decimaltoanybase(int n)
+    int decimaltoanybase(int n,int b)
     {
        int ans=0,pow=1;
        while(n!=0)
        {
-           int sum=n%2;
-           n=n/2;
-           
+           int sum=n%b;
+           n=n/b;
+           ans+=sum*pow;
+           pow=pow*10;
        }
+       return  ans;
+    }
+    int anybasetobase(int n,int b1,int b2)
+    {
+     int ans=binarytodecimal(n,b1);
+        System.out.println(ans);
+     int ans1=decimaltoanybase(ans,b2);
+     return ans1;
+    }
+    int binarytodecimal(int n,int b1)
+    {
+        int ans=0,pow=1;
+        while(n!=0)
+        {
+         int sum=n%10;
+         n=n/10;
+         ans+=sum*pow;
+         pow=pow*b1;
+
+        }
+        return ans;
+    }
+    int decimaltobinary(int n,int b1)
+    {
+        int ans=0,pow=1;
+      while(n!=0)
+      {
+          int sum=n%b1;
+          n=n/b1;
+          ans+=sum*pow;
+          pow=pow*10;
+
+
+      }
+      return  ans;
+    }
+    int decimaladdition1(int base,int a,int b)
+    {
+        int ans=0,carry=0,pow=1;
+        while(carry!=0||a!=0||b!=0)
+        {
+            int sum=carry+(a%10)+(b%10);
+
+            carry=sum/base;
+            sum=sum%base;
+            a=a/10;
+            b=b/10;
+            ans+=sum*pow;
+            pow=pow*10;
+
+
+        }
+        return  ans;
+    }
+    int decimalsubtraction(int n,int m,int b)
+    {
+        int borrow=0,ans=0,pow=1;
+        while(n!=0)
+        {
+            int sum=borrow+(n%10)-(m%10);
+            n=n/10;
+            m=m/10;
+            if(sum<0)
+            {
+                sum=b+sum;
+                //System.out.println(sum);
+                borrow=-1;
+
+            }
+            else
+            {
+                borrow=0;
+            }
+            ans+=sum*pow;
+            pow*=10;
+        }
+        return  ans;
+    }
+    int multiply(int n,int m,int b)
+    {
+        int ans=0,pow=1;
+       while(m!=0)
+       {
+           int sum=decimalmultiply(n,m%10);
+           m=m/10;
+           ans=ans+decimaladdition(ans,sum*pow);
+           pow=pow*10;
+       }
+       return  ans;
+    }
+    int decimalmultiply(int n,int m,int b)
+    {
+        int ans=0,pow=1,carry=0;
+        while(n!=0)
+        {
+            int sum=carry+((n%10)*m);
+            n=n/10;
+            sum=sum%10;
+            carry=sum/b;
+            ans+=sum*pow;
+            pow*=10;
+
+        }
+        return ans;
     }
 
 
